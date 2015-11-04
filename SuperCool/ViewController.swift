0fdb844549b54aa38406d86d56cb71328a6fb29b
Mark S Stephenson <Stephenson@marks-imac.home>
2015-11-04 10:38:41 +0000

    added files for supercool

---
+++ SuperCool/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  SuperCool
+//
+//  Created by Mark S Stephenson on 29/10/2015.
+//  Copyright © 2015 Mark Stephenson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var coolLogo: UIImageView!
+    @IBOutlet weak var coolBg: UIImageView!
+    @IBOutlet weak var uncoolButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func loadCharlotteApp(sender: AnyObject) {
+        coolLogo.hidden = false
+        coolBg.hidden = false
+        uncoolButton.hidden = true
+        
+    }
+   
+}
+
